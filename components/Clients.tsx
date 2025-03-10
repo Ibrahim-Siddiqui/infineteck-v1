54de59e62475c49fe2d34d986cc8912c813c9c0b
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-03-11 00:33:30 +0500

    Made some changes to breaking imports.

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
+import { testimonials } from "@/data";
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 import Image from "next/image";
 
@@ -27,7 +27,7 @@ const Clients = () => {
         </div>
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-          {companies.map((company) => (
+          {/* {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
                 <Image
@@ -46,7 +46,7 @@ const Clients = () => {
                 />
               </div>
             </React.Fragment>
-          ))}
+          ))} */}
         </div>
       </div>
     </section>
