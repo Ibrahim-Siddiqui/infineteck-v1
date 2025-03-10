c7dcbbb5ba25cc792a20892d9048862c1e841ce7
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-03-11 00:25:33 +0500

    Added portfolio page and its components.

---
+++ app/portfolio/components/PortfolioCards.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import { FocusCards } from '@/components/ui/FocusCards'
+
+// const PortfolioCards = ({data}:) => {
+//   return (
+//     <div className="relative my-[5rem]">
+//             {/* <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
+//               Text Logo Portfolio
+//             </h1> */}
+
+//           <FocusCards cards={data} loadMore={isTextLogo} />
+//           {data > 3 ? (
+//               <div className="flex justify-center my-5">
+//               <button
+//                 className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+//                 onClick={(e) => setIsTextLogo(!isTextLogo)}
+//               >
+//                 {!isTextLogo ? <>Show More</> : <>Show Less</>}
+//               </button>
+//             </div>
+//                 </div>
+       
+//   )
+// }
+
+// export default PortfolioCards
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,176 +1,264 @@
 "use client";
 
 import React, { useState } from "react";
-
-import Hero4 from "@/components/Hero4";
 import Footer from "@/components/Footer";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
 import { FocusCards } from "@/components/ui/FocusCards";
 import { VideoCards } from "@/components/ui/VideoCards";
-
-import { navItems } from "@/data";
-import { UIUXPortfolioData } from "@/data";
-import { textLogoPortfolioData } from "@/data";
-import { gameDevPortfolioData } from "@/data";
-import { shopifyPortfolioData } from "@/data";
-import { mascotPortfolioData } from "@/data";
-import { socialMediaPortfolioData } from "@/data";
-import { videoPortfolioData } from "@/data";
+import {
+  UIUXPortfolioData,
+  textLogoPortfolioData,
+  gameDevPortfolioData,
+  shopifyPortfolioData,
+  mascotPortfolioData,
+  socialMediaPortfolioData,
+  videoPortfolioData,
+} from "@/data";
+import Navbar from "@/components/ui/Navbar";
 
 const Home = () => {
-  const [isTextLogo, setIsTextLogo] = useState(false);
+  const [isTextLogo, setIsTextLogo] = useState(true);
   const [isUIUX, setIsUIUX] = useState(false);
   const [isGameDev, setIsGameDev] = useState(false);
   const [isShopify, setIsShopify] = useState(false);
   const [isMascot, setIsMascot] = useState(false);
   const [isSocialMedia, setIsSocialMedia] = useState(false);
   const [isVideoEditing, setIsVideoEditing] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
+  const handleButtonClick = (stateSetter: (value: boolean) => void) => {
+    setIsTextLogo(false);
+    setIsGameDev(false);
+    setIsMascot(false);
+    setIsUIUX(false);
+    setIsVideoEditing(false);
+    // setSeo(false);
+    setIsShopify(false);
+    setIsSocialMedia(false);
+    setShowMore(false);
+
+    stateSetter(true);
+  };
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero4 />
-        {/* <h1 className="heading">BRANDING</h1>  */}
-        {/* <br />
-        <br /> */}
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      {/* <div className=""> */}
+      <Navbar />
 
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Text Logo Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={textLogoPortfolioData} loadMore={isTextLogo} />
-        {textLogoPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsTextLogo(!isTextLogo)}
-            >
-              {!isTextLogo ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            UI/UX Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={UIUXPortfolioData} loadMore={isUIUX} />
-        {UIUXPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsUIUX(!isUIUX)}
-            >
-              {!isUIUX ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Game Development Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={gameDevPortfolioData} loadMore={isGameDev} />
-        {gameDevPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsGameDev(!isGameDev)}
-            >
-              {!isGameDev ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Mascot Logo Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={mascotPortfolioData} loadMore={isMascot} />
-        {mascotPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsMascot(!isMascot)}
-            >
-              {!isMascot ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Socail Media Posts Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={socialMediaPortfolioData} loadMore={isSocialMedia} />
-        {socialMediaPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsSocialMedia(!isSocialMedia)}
-            >
-              {!isSocialMedia ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Video Editing Portfolio
-          </h1>
-        </div>
-        <VideoCards cards={videoPortfolioData} loadMore={isVideoEditing} />
-        {videoPortfolioData.length > 4 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsVideoEditing(!isVideoEditing)}
-            >
-              {!isVideoEditing ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-
-        <div className="relative my-[5rem]">
-          <h1 className="text-white text-4xl md:text-5xl text-center font-bold">
-            Shopify UI/UX Portfolio
-          </h1>
-        </div>
-        <FocusCards cards={shopifyPortfolioData} loadMore={isShopify} />
-        {shopifyPortfolioData.length > 3 ? (
-          <div className="flex justify-center my-5">
-            <button
-              className="bg-blue-900 p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
-              onClick={(e) => setIsShopify(!isShopify)}
-            >
-              {!isShopify ? <>Show More</> : <>Show Less</>}
-            </button>
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-
-        <Footer />
+      <div className="my-10">
+        <h1 className="text-5xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color">
+          Our Portfolio
+        </h1>
       </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:flex lg:flex-wrap gap-5 justify-center text-lg">
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsTextLogo)}
+        >
+          Logo Design
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsMascot)}
+        >
+          Mascot Logos
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsUIUX)}
+        >
+          UI / UX
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsVideoEditing)}
+        >
+          Video & Animation
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsShopify)}
+        >
+          Shopify Store
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsSocialMedia)}
+        >
+          Social Media
+        </button>
+        <button
+          className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+          onClick={() => handleButtonClick(setIsGameDev)}
+        >
+          Game Development
+        </button>
+        {/*  <button
+            className="rounded-lg px-4 py-2 text-white transition-all bg-primary-color hover:scale-105"
+            onClick={() => handleButtonClick(setSocialMedia)}
+          >
+            Social Media Management
+          </button> */}
+      </div>
+      {isTextLogo && (
+        <>
+          <div className="relative mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Text Logo Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={textLogoPortfolioData} loadMore={showMore} />
+          {textLogoPortfolioData.length > 3 ? (
+            <div className="flex justify-center mb-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+
+      {isUIUX && (
+        <>
+          <div className=" mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              UI/UX Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={UIUXPortfolioData} loadMore={showMore} />
+          {UIUXPortfolioData.length > 3 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+      {isGameDev && (
+        <>
+          <div className="relative mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Game Development Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={gameDevPortfolioData} loadMore={showMore} />
+          {gameDevPortfolioData.length > 3 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+
+      {isMascot && (
+        <>
+          <div className="relative mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Mascot Logo Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={mascotPortfolioData} loadMore={showMore} />
+          {mascotPortfolioData.length > 3 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+      {isSocialMedia && (
+        <>
+          <div className="relative mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Socail Media Posts Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={socialMediaPortfolioData} loadMore={showMore} />
+          {socialMediaPortfolioData.length > 3 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+      {isVideoEditing && (
+        <>
+          <div className="relative my-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Video Editing Portfolio
+            </h1>
+          </div>
+          <VideoCards cards={videoPortfolioData} loadMore={showMore} />
+          {videoPortfolioData.length > 4 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+
+      {isShopify && (
+        <>
+          <div className="relative mt-[5rem]">
+            <h1 className="text-black text-4xl md:text-5xl text-center font-bold">
+              Shopify UI/UX Portfolio
+            </h1>
+          </div>
+          <FocusCards cards={shopifyPortfolioData} loadMore={showMore} />
+          {shopifyPortfolioData.length > 3 ? (
+            <div className="flex justify-center my-[5rem]">
+              <button
+                className="bg-primary-color p-4 rounded-lg text-white text-xl hover:scale-105 transition-all ease-in-out"
+                onClick={(e) => setShowMore(!showMore)}
+              >
+                {!showMore ? <>Show More</> : <>Show Less</>}
+              </button>
+            </div>
+          ) : (
+            <div className=""></div>
+          )}
+        </>
+      )}
+      <Footer />
+      {/* </div> */}
     </main>
   );
 };
