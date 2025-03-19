f38fc011d6dbb9875315ea556179dc26ff66003a
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-03-19 08:22:19 +0500

    Removed VidStack player and added HTML video tag and its specifications.

--- components/VideoHero.tsx
+++ components/VideoHero.tsx
@@ -1,6 +1,4 @@
-// "use client";
-import "@vidstack/react/player/styles/base.css";
-import "@vidstack/react/player/styles/plyr/theme.css";
+"use client";
 import React from "react";
 
 const VideoHero = () => {
@@ -26,7 +24,6 @@ const VideoHero = () => {
       <section className="relative h-screen sm:h-[70vh] xl:h-[80vh] flex flex-col items-center justify-center text-center text-white ">
         <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
           <video
-            // controls={false}
             className="min-w-full min-h-full absolute object-cover"
             autoPlay
             muted
@@ -62,50 +59,6 @@ const VideoHero = () => {
           </div>
         </div>
       </section>
-
-      {/* <section className="relative h-[55vw] flex flex-col items-center justify-center text-center text-white">
-        <div className="video-docker absolute top-0 left-0 w-full h-[55vw] overflow-hidden">
-          <video
-            controls={false}
-            className="min-w-full min-h-full h-screen absolute object-cover"
-            autoPlay
-            muted
-            loop
-          >
-            <source
-              
-              src="/hero-video.mp4"
-              type="video/mp4"
-            />
-          </video>
-          
-        </div> */}
-      {/* video-content space-y-2 z-10  text-center */}
-      {/* pt-[10vh] sm:pt-[20vh] md:[25vh] lg:pt-[20vh] xl:pt-[35vh] */}
-      {/* <div className="relative z-10 flex flex-col justify-center items-center">
-          <h1 className="text-xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold tracking-tight text-center text-white ">
-            <span className="block xl:inline">
-              <span className="block text-center">Transforming Concepts</span>
-              <span className="lg:pl-3 bg-gradient-to-l from-secondary-color to-primary-color bg-clip-text text-transparent">
-                Into Seamless User Experiences.
-              </span>
-            </span>
-          </h1>
-          <p className="justify-center max-w-xl hidden md:block md:max-w-2xl lg:max-w-4xl text-[10px] sm:text-lg lg:text-2xl  text-white">
-            At Infineteck, we empower small, medium, and large businesses to
-            dominate their industries with cutting-edge digital solutions. From
-            crafting unforgettable brands to scaling revenue with AI-driven
-            strategies
-          </p> */}
-      {/* <a
-            // href="/CC Profile-2024.pdf"
-            className="mt-[2%] lg:mt-[5%] px-2 lg:px-8 py-2 md:py-4 md:px-8 text-[8px] sm:text-sm md:text-lg rounded-md border border-transparent shadow bg-primary-color hover:scale-105 transition-all duration-200 font-medium text-white cursor-pointer"
-            // download
-          >
-            Download Our Company Profile
-          </a>
-        </div> */}
-      {/* </section> */}
 
       <style>{css}</style>
     </>
