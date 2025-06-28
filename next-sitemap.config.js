d5139bd502bd6e6a1a614d3b639f966f09a47380
Ibrahim Ahmed Siddiqui <107644916+Ibrahim-Siddiqui@users.noreply.github.com>
2025-06-28 21:32:53 +0500

    configration of site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,10 @@
 module.exports = {
     siteUrl: 'https://infineteck.com',
     generateRobotsTxt: true,
-    sitemapSize: 5000,
     changefreq: 'weekly',
     priority: 0.7,
+    exclude: [],
+    experimental: {
+        appDir: true,
+    },
 };
