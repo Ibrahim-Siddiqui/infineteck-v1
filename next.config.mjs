8f39dd4f3f879fcaabc5ef02ee086cd4a87ff681
Ibrahim Ahmed Siddiqui <107644916+Ibrahim-Siddiqui@users.noreply.github.com>
2025-06-28 19:32:26 +0500

    number changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+       unoptimized: true
+    },
+      // Add this for static export support
+  output: 'export',
+  // Optional but safer: avoid `.next/export` conflicts
+  trailingSlash: true, // helps with static hosting
+};
