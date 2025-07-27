e7267983b1011aee65fcbe33508c629b6809353e
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-07-27 20:19:39 +0500

    Shortened app title for seo purposes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ const open_sans = Open_Sans({
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Digital Marketing, E-commerce & AI Development Experts | Infineteck",
+  title: "Infineteck | Marketing, E-commerce & AI Development Experts",
   description:
     "Infineteck delivers expert web and app development, AI automation, digital marketing, and e-commerce solutions to help startups and enterprises grow online in the UK, USA, UAE, and globally.",
   openGraph: {
