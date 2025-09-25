be44bf4f9cbe6d63a7bad66609e8198ce5cb9858
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-09-25 08:52:37 +0500

    Added changes to web dev service page.

--- app/services/web-development/page.tsx
+++ app/services/web-development/page.tsx
@@ -4,10 +4,17 @@ import headerImg from "@/public/web-dev-service-min.jpg";
 import VideoSection from "../components/VideoSection";
 import NewNavbar from "@/components/NewNavbar";
 import Footer from "@/components/Footer";
-import FeatureSection from "../components/FeatureSection";
 import FeatureCards from "./FeatureCards";
+import LongScreenshot from "@/components/LongScreenshot";
+import Plans from "@/app/pricing/components/Plans";
+import ContactForm from "@/components/ContactForm";
 import { WebVideoData } from "./data";
-import { WebFeaturesData } from "./data";
+import { UIUXPortfolioData } from "@/data";
+import { webDevPlans } from "@/app/pricing/data";
+import Image from "next/image";
+import Image2 from "./3.png";
+import Image1 from "./2.png";
+import Image3 from "./1.png";
 
 const WebDevelopment = () => {
   return (
@@ -22,13 +29,64 @@ const WebDevelopment = () => {
           commitment."
       />
       <VideoSection data={WebVideoData} />
-      <FeatureSection
-        data={WebFeaturesData}
-        initialHeading="Transform Your"
-        specialText="Online Presence"
-        endHeading="with Expert Web Development"
-      />
+      <div className="my-5 flex flex-col justify-center items-center">
+        <h2 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+          Our Process
+        </h2>
+        {/* <p>Equipped with latest tools, our team deliver impactful solutions designed to grow your business</p> */}
+        <Image
+          src={Image1}
+          className=""
+          width={3000}
+          height={1}
+          alt="service image"
+        />
+      </div>
+
+      <h1 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+        Our Portfolio
+      </h1>
+      <div className="flex justify-center overflow-y-auto scrollbar-hide my-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-[2%] sm:gap-x-[10%] lg:gap-y-[5%]">
+          {UIUXPortfolioData.map((item) => (
+            <LongScreenshot key={item.id} path={item.src} />
+          ))}
+        </div>
+      </div>
+      <div className="my-[15%] md:mt-[10%] md:mb-[5%] flex flex-col justify-center items-center">
+        <h2 className="text-3xl md:text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+          Our Tech Stack
+        </h2>
+        <p className="text-md md:text-lg text-center">
+          Equipped with latest tools, our team deliver impactful solutions
+          designed to grow your business
+        </p>
+        <Image
+          src={Image2}
+          className=""
+          width={1920}
+          height={1}
+          alt="service image"
+        />
+      </div>
       <FeatureCards />
+      <div className="my-5 flex flex-col justify-center items-center">
+        {" "}
+        <Image
+          src={Image3}
+          className=""
+          width={2560}
+          height={1}
+          alt="service image"
+        />
+      </div>
+      <div className="mt-5 flex flex-col justify-center items-center">
+        <h2 className="text-4xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color py-5">
+          Our Pricing
+        </h2>
+        <Plans plans={webDevPlans} />
+      </div>
+      <ContactForm />
       <Footer />
     </>
   );
--- components/HomeReviews.tsx
+++ components/HomeReviews.tsx
@@ -4,22 +4,35 @@ import React from "react";
 const HomeReviews = () => {
   return (
     <div className="flex flex-col items-center">
-        <div className="">
-
-            <h2 className="py-5 text-2xl md:text-5xl text-center font-bold leading-5">Your Voice Matters, Review <span className="bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color">Infineteck</span></h2>
-            <p className="text-md md:text-lg text-center">Loved working with Infineteck? Share your experience and help others discover the magic of Infineteck</p>
-        </div>
+      <div className="">
+        <h2 className="py-5 text-2xl md:text-5xl text-center font-bold leading-5">
+          Your Voice Matters, Review{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color">
+            Infineteck
+          </span>
+        </h2>
+        <p className="text-md md:text-lg text-center">
+          Loved working with Infineteck? Share your experience and help others
+          discover the magic of Infineteck
+        </p>
+      </div>
       <div className="flex items-center md:gap-[5%]">
-        <a href="https://clutch.co/profile/infineteck-0">
+        <a href="https://clutch.co/profile/infineteck-0" target="_blank">
           <Image src="/clutch.webp" width={250} height={1} alt="Clutch Logo" />
         </a>
-        <a href="https://share.google/H6AuSDgjJg0E6AIqo">
+        <a href="https://share.google/H6AuSDgjJg0E6AIqo" target="_blank">
           <Image src="/google.webp" width={250} height={1} alt="Clutch Logo" />
         </a>
-        <a href="https://www.yell.com/biz/infineteck-bury-st-edmunds-10967978">
+        <a
+          href="https://www.yell.com/biz/infineteck-bury-st-edmunds-10967978"
+          target="_blank"
+        >
           <Image src="/yell.png" width={250} height={1} alt="Clutch Logo" />
         </a>
-        <a href="https://uk.trustpilot.com/review/infineteck.com">
+        <a
+          href="https://uk.trustpilot.com/review/infineteck.com"
+          target="_blank"
+        >
           <Image
             src="/trust-pilot.png"
             width={250}
