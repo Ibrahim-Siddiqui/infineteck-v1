d93ee6011fd6f8a4686abf6e1329e31dea745a64
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-10-15 17:11:22 +0500

    Modified AI Automation service page with new sections and images.

--- app/services/ai-automation/page.tsx
+++ app/services/ai-automation/page.tsx
@@ -8,6 +8,12 @@ import FeatureSection from "../components/FeatureSection";
 import FeatureCards from "./FeatureCards";
 import { AIVideoData } from "@/app/services/ai-automation/data";
 import { AIFeaturesData } from "@/app/services/ai-automation/data";
+import Image from "next/image";
+import Image1 from "./2.png";
+import Image2 from "./3.png";
+import Image3 from "./1.png";
+import Plans from "@/app/pricing/components/Plans";
+import ContactForm from "@/components/ContactForm";
 
 const AppDevelopment = () => {
   return (
@@ -19,13 +25,62 @@ const AppDevelopment = () => {
         text="Leverage the power of AI to automate repetitive tasks, enhance decision-making, and unlock new possibilities. Our AI-driven automation solutions help businesses improve efficiency, reduce costs, and stay ahead of the competition."
       />
       <VideoSection data={AIVideoData} />
-      <FeatureSection
+      <div className="hidden my-5 md:flex flex-col justify-center items-center">
+        <h2 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+          Our Process
+        </h2>
+        {/* <p>Equipped with latest tools, our team deliver impactful solutions designed to grow your business</p> */}
+        <Image
+          src={Image1}
+          className=""
+          width={3000}
+          height={1}
+          alt="service image"
+        />
+      </div>
+      {/* <FeatureSection
         data={AIFeaturesData}
         initialHeading=""
         specialText="Unlock Efficiency "
         endHeading="with AI-Powered Automation"
-      />
+      /> */}
+
+      <div className="my-[15%] md:mt-[10%] md:mb-[5%] flex flex-col justify-center items-center">
+        <h2 className="text-3xl md:text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+          Our Tech Stack
+        </h2>
+        <p className="text-md md:text-lg text-center">
+          Equipped with latest tools, our team deliver impactful solutions
+          designed to grow your business
+        </p>
+        <Image
+          src={Image2}
+          className=""
+          width={1920}
+          height={1}
+          alt="service image"
+        />
+      </div>
+
       <FeatureCards />
+
+      <div className="hidden my-5 md:flex flex-col justify-center items-center">
+        {" "}
+        <Image
+          src={Image3}
+          className=""
+          width={2560}
+          height={1}
+          alt="service image"
+        />
+      </div>
+      {/* <div className="mt-5 flex flex-col justify-center items-center">
+        <h2 className="text-4xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color py-5">
+          Our Pricing
+        </h2>
+        <Plans plans={Plans} />
+      </div> */}
+      <ContactForm />
       <Footer />
     </>
   );
