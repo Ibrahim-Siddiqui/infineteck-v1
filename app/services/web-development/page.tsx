9a92150f10271caf7f1d855b23f4281591297aad
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-10-15 17:36:40 +0500

    Modified web development page with new images and service pages.

--- app/services/web-development/page.tsx
+++ app/services/web-development/page.tsx
@@ -12,8 +12,8 @@ import { WebVideoData } from "./data";
 import { UIUXPortfolioData } from "@/data";
 import { webDevPlans } from "@/app/pricing/data";
 import Image from "next/image";
-import Image2 from "./3.png";
 import Image1 from "./2.png";
+import Image2 from "./3.png";
 import Image3 from "./1.png";
 
 const WebDevelopment = () => {
@@ -29,7 +29,7 @@ const WebDevelopment = () => {
           commitment."
       />
       <VideoSection data={WebVideoData} />
-      <div className="my-5 flex flex-col justify-center items-center">
+      <div className="hidden my-5 md:flex flex-col justify-center items-center">
         <h2 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
           Our Process
         </h2>
@@ -43,11 +43,21 @@ const WebDevelopment = () => {
         />
       </div>
 
-      <h1 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color my-5">
+      <h1 className="text-3xl md:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-secondary-color to-primary-color mt-5 mb-2">
         Our Portfolio
       </h1>
-      <div className="flex justify-center overflow-y-auto scrollbar-hide my-5">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-[2%] sm:gap-x-[10%] lg:gap-y-[5%]">
+      <p className="md:hidden text-xs text-center font-semibold">
+        (Tap on the image for it to scroll)
+      </p>
+      <p className="hidden md:block text-md text-center font-semibold">
+        (Hover over the image for it to scroll)
+      </p>
+      <div
+        className="flex justify-center overflow-y-auto my-5 h-[650vh] md:h-[300vh] 2xl:h-[350%]"
+        style={{ scrollbarWidth: "thin" }}
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[2%] sm:gap-x-[10%] lg:gap-y-[5%]">
+          {/* sm:gap-x-[10%] */}
           {UIUXPortfolioData.map((item) => (
             <LongScreenshot key={item.id} path={item.src} />
           ))}
@@ -70,7 +80,7 @@ const WebDevelopment = () => {
         />
       </div>
       <FeatureCards />
-      <div className="my-5 flex flex-col justify-center items-center">
+      <div className="hidden my-5 md:flex flex-col justify-center items-center">
         {" "}
         <Image
           src={Image3}
