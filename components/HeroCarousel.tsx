a49d9a98e84af65d7242b94ab9b780eda5fd9894
Bilal Sohail Mirza <bsohail420@gmail.com>
2025-10-25 08:20:37 +0500

    Added hero carousel component.

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -10,13 +10,21 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import { partners } from "@/data";
 import { motion } from "framer-motion";
 
 const HeroCarousel = () => {
+  const partners = [
+    { img: "/google-partner-logo.png" },
+    { img: "/Marriott_hotels_logo14.png" },
+    { img: "/hostinger8547.jpg" },
+    { img: "/titan-logo-.png" },
+    { img: "/Met-Business-Partners.webp" },
+    { img: "/shopify-partner-1.png" },
+  ];
+
   return (
     <div
-      className="z-[-99] mx-[5%] shadow-sm"
+      className="px-[5%] shadow-sm"
       // initial={{
       //   opacity: 0,
       //   translateY: 100,
@@ -33,7 +41,6 @@ const HeroCarousel = () => {
       //   margin: "-250px",
       // }}
     >
-
       <Carousel
         opts={{
           direction: "ltr",
@@ -41,15 +48,18 @@ const HeroCarousel = () => {
         }}
         plugins={[
           Autoplay({
-            delay: 1000,
+            delay: 1500,
           }),
         ]}
         className=""
       >
-        <CarouselContent className="">
+        <CarouselContent className="flex items-center">
           {partners.map((partner) => (
-            <CarouselItem key={partner.id} className="basis-1/3 md:basis-1/4 ">
-              <div className="flex justify-center items-end rounded-lg">
+            <CarouselItem
+              key={partner.img}
+              className="basis-1/2 sm:basis-1/3 lg:basis-1/4 xl:basis-1/5 "
+            >
+              <div className="flex justify-center items-center rounded-lg h-[200px] w-[200px]">
                 <Image
                   src={partner.img}
                   width={180}
@@ -62,7 +72,6 @@ const HeroCarousel = () => {
           ))}
         </CarouselContent>
       </Carousel>
-     
     </div>
   );
 };
